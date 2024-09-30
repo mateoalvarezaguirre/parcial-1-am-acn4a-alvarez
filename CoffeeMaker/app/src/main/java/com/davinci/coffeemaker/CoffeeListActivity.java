3cb08453f2bf2b30595ff4d96c336d3415f4488c
mateoalvarezaguirre <mateo.alvarez@davinci.edu.ar>
2024-09-29 22:20:59 -0300

    feature: first view with "poppins" typo

--- CoffeeMaker/app/src/main/java/com/davinci/coffeemaker/CoffeeListActivity.java
+++ CoffeeMaker/app/src/main/java/com/davinci/coffeemaker/CoffeeListActivity.java
@@ -1,8 +1,6 @@
 package com.davinci.coffeemaker;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CoffeeListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_coffee_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void showCoffeeList(View view) {
-        Intent coffeeListIntent = new Intent(this, CoffeeListActivity.class);
-        startActivity(coffeeListIntent);
     }
 }
